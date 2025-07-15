f6a8a64ad3cdf522f8b16badb1a6cc0700f9a337
Amirul <amirulal73@gmail.com>
2025-07-15 17:32:27 +0800

    Passing data from controller to view

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,14 +6,24 @@ class Pages extends BaseController
 {
     public function index()
     {
-        echo view('layout/header');
+        $data = [
+            'title' => 'Home | Cuci',
+            'test' => ['satu', 'dua', 'tiga'],
+        ];
+
+        echo view('layout/header', $data);
         echo view('pages/home');
         echo view('layout/footer');
     }
 
     public function about()
     {
-        echo view('layout/header');
+
+        $data = [
+            'title' => 'About | Cuci',
+        ];
+
+        echo view('layout/header', $data);
         echo view('pages/about');
         echo view('layout/footer');
     }
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -2,6 +2,9 @@
     <div class="row">
         <div class="col">
             <h1>Hello, world!</h1>
+            <?php
+            dd($test);
+            ?>
         </div>
     </div>
 </div>
