4e4662467ab83e24b6238ef1a7b78630717f2b9b
Amirul <amirulal73@gmail.com>
2025-07-19 14:42:46 +0800

    Add form on create page

--- app/Views/comics/create.php
+++ app/Views/comics/create.php
@@ -2,9 +2,42 @@
 
 <?= $this->section('content') ?>
 <div class="container">
-    <h1>Create comic page</h1>
+    <h1 class="fs-4">Create comic page</h1>
     <div class="col">
-        <div class="row"></div>
+        <div class="row">
+            <form action="/komik/save" method="POST">
+                <?= csrf_field(); ?>
+                <div class="form-group row mb-3">
+                    <label for="name" class="col-sm-2 col-form-label">Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" placeholder="comic name" autofocus>
+                    </div>
+                </div>
+                <div class="form-group row mb-3">
+                    <label for="writer" class="col-sm-2 col-form-label">Writer</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="writer" placeholder="comic writer">
+                    </div>
+                </div>
+                <div class="form-group row mb-3">
+                    <label for="editor" class="col-sm-2 col-form-label">Editor</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="editor" placeholder="comic editor">
+                    </div>
+                </div>
+                <div class="form-group row mb-3">
+                    <label for="cover" class="col-sm-2 col-form-label">Image Cover</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="cover" placeholder="comic cover">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 <?= $this->endSection() ?>
